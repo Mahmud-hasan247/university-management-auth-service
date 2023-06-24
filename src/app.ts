36d73b55b10ac4472f97f6db4dc9d31e104f0eca
mahmudhasan <mahmudhasan.akij@gmail.com>
2023-06-24 22:54:33 +0600

    [DONE] create user interface and model

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ const app: Application = express()
 app.use(cors())
 
 // parser
-           app.use(express.json())
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req: Request, res: Response) => {
