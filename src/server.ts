e3ee0f2f5f7d782a991b408170a14e242f2faabd
mahmudhasan <mahmudhasan.akij@gmail.com>
2023-06-24 22:14:51 +0600

    husky check

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/index";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
 
-const { port, database_url } = config;
+const { port, database_url } = config
 
 async function main() {
   try {
-    await mongoose.connect(database_url as string);
-    console.log("Database is Connected successfully");
+    await mongoose.connect(database_url as string)
+    console.log('Database is Connected successfully')
     app.listen(port, () => {
-      console.log(`University Application is listening on port ${port}`);
-    });
+      console.log(`University Application is listening on port ${port}`)
+    })
   } catch (error) {
-    console.error("Failed to connect database", error);
+    console.error('Failed to connect database', error)
   }
 }
 
-main();
+main()
