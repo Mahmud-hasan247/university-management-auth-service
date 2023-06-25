882a4c5eb606422e6def7812bc3e8cbf5bc1a260
mahmudhasan <mahmudhasan.akij@gmail.com>
2023-06-26 00:40:19 +0600

    [DONE] global error handling

---
+++ src/errors/validationErrorHandler.ts
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+import { IGenericErrorMessages } from '../interfaces/validationErrorMessage'
+import { IGenericErrorResponse } from '../interfaces/errorResponse'
+
+const validationErrorHandler = (err: mongoose.Error.ValidationError) => {
+  const errorMessages: IGenericErrorMessages[] = Object.values(err.errors).map(
+    el => {
+      return {
+        path: el?.path,
+        message: el?.message,
+      }
+    }
+  )
+
+  const errorResponse: IGenericErrorResponse = {
+    statusCode: 400,
+    message: 'Validation Error',
+    errorMessages,
+  }
+
+  return errorResponse
+}
+
+export default validationErrorHandler
